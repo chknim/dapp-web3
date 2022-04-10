0be51ef20e7560ffaf1b8e9d392b936968254f9a
chknim <cnimmana@gmail.com>
2022-04-09 23:19:53 -0400

    Finalize first web3 attempt

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+import { ethers } from "ethers";
+
+export default function App() {
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Please install MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      fetchGreetings();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchGreetings = async () => {
+    // WARNING: this is a dummy contract address generated on local network.
+    // Ideally the address must not be used outside of this testing environment on local network.
+    let contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert("Please install MetaMask!");
+      return;
+    }
+
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      contractAddress,
+      Greeter.abi,
+      provider
+    );
+
+    const greeting = await contract.greet();
+    console.log(greeting);
+  };
+
+  useEffect(() => {
+    connectWallet();
+  }, []);
+
+  return <div></div>;
+}
