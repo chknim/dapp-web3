67fef93fec850bd1a8b4d545da23ca414e97fd9d
chknim <cnimmana@gmail.com>
2022-04-10 16:08:12 -0400

    Dynamic greeting get/set

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { useEffect } from "react";
-import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
-import { ethers } from "ethers";
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+
+    // WARNING: this is a dummy contract address generated on local network after deploying our code.
+    // Ideally the address must not be used outside of this testing environment on local network.
+const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
 export default function App() {
+  const [greeting, setGreetingValue] = useState();
   const connectWallet = async () => {
     try {
-      const { ethereum } = window;
-
-      if (!ethereum) {
+      if (!window.ethereum) {
         alert("Please install MetaMask!");
         return;
       }
 
-      const accounts = await ethereum.request({
+      const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
 
@@ -24,31 +27,52 @@ export default function App() {
   };
 
   const fetchGreetings = async () => {
-    // WARNING: this is a dummy contract address generated on local network.
-    // Ideally the address must not be used outside of this testing environment on local network.
-    let contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
-    const { ethereum } = window;
-
-    if (!ethereum) {
+    if (!window.ethereum) {
       alert("Please install MetaMask!");
       return;
     }
 
-    const provider = new ethers.providers.Web3Provider(ethereum);
-    const signer = provider.getSigner();
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
     const contract = new ethers.Contract(
       contractAddress,
       Greeter.abi,
       provider
     );
 
-    const greeting = await contract.greet();
-    console.log(greeting);
+    const data = await contract.greet();
+    if (data !== greeting) {
+      setGreetingValue(data);
+    }
+    console.log(data);
+  };
+
+  const setGreeting = async () => {
+    if (!greeting) return;
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        contractAddress,
+        Greeter.abi,
+        signer
+      );
+      const transaction = await contract.setGreeting(greeting);
+      await transaction.wait();
+    }
   };
 
   useEffect(() => {
     connectWallet();
   }, []);
 
-  return <div></div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={fetchGreetings}>Fetch Greeting</button>
+        <button onClick={setGreeting}>Set Greeting</button>
+        <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
+      </header>
+      <div>{greeting}</div>
+    </div>
+  );
 }
